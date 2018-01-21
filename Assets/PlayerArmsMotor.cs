90f5a52c6100fe83372bbb182824a07021e63577
Cory Brown <Cory.Brown@sbcs.com>
2018-01-20 20:07:41 -0500

    Push
    Animations for hands

---
+++ Assets/PlayerArmsMotor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerArmsMotor : MonoBehaviour {
+
+
+    public Animator anim;
+
+    public GameObject sword;
+
+    bool Activate;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim.SetBool("isIdleUnarmed", true);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(sword.activeSelf == true)
+        {
+            anim.SetBool("isIdleUnarmed", false);
+        }
+
+        if(sword.activeSelf == false)
+        {
+            anim.SetBool("isIdleUnarmed", true);
+        }
+
+        if (sword.activeSelf == true && Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("isStabbingSword", true);
+        }
+        else
+        {
+            anim.SetBool("isStabbingSword", false);
+        }
+    }
+}
